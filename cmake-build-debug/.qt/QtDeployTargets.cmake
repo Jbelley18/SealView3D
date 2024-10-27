a1211c7987b334f7902fcaf6cc81f32086dcba91
Justin Belley <jbelley@wgu.edu>
2024-10-27 05:46:03 -0400

    Remove unnecessary files and folders from repository

--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_3DViewer_FILE C:/Users/justi/CLionProjects/3DViewer/cmake-build-debug/bin/3DViewer.exe)
-set(__QT_DEPLOY_TARGET_3DViewer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_3DViewer_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6OpenGLWidgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
